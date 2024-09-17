a82705d750b672e024271d0f9904b1f55ff6f794
BlackSnake0904 <117591705+BlackSnake0904@users.noreply.github.com>
2024-09-17 16:53:27 +0200

    Add project files.

---
+++ Design a Stack/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Design_a_Stack;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Zasobnik stack = new Zasobnik();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+
+
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop()); 
+Console.WriteLine(stack.Pop());
---
+++ Design a Stack/Zasobnik.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_a_Stack
+{
+
+    public class Zasobnik
+    {
+
+        public readonly List<int> ListObjektov = new List<int>();
+
+       
+
+        public void Push(int obj)
+        {
+
+            ListObjektov.Add(obj);
+
+        }
+
+        public int Pop()
+        {
+
+            if (ListObjektov.Capacity >= 1)
+            {
+                var pom = ListObjektov[ListObjektov.Count - 1];
+                ListObjektov.RemoveAt(ListObjektov.Count - 1);
+                return pom;
+            }
+            else
+            {
+                throw new InvalidOperationException("Zasobnik je prazdny");
+
+            }
+
+
+
+
+
+        }
+    }
+}
+
+
+
